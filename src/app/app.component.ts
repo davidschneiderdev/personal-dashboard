16ef0dd437cb246c8ba52017be66e41373d7eb2c
David Schneider <david.jay.schneider@gmail.com>
2022-10-04 15:59:09 -0400

    Additional transition animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { transition, trigger, animate, style, query } from '@angular/animations';
+import { transition, trigger, animate, style, query, group } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -10,29 +10,36 @@ import { transition, trigger, animate, style, query } from '@angular/animations'
     trigger('routeAnim', [
       transition('* => *', [
 
-        query(':enter', [
-          style({ opacity: 0 })
-        ], { optional: true }),
-
-        query(':leave', [
+        query(':enter, :leave', [
           style({
-            display: 'block'
-          }),
-          animate(1000, style ({
-            opacity: 0
-          }))
-        ], { optional: true }),
-
-        query(':enter', [
-          style({
-            opacity: 0,
-            display: 'block',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
             height: '100%'
-          }),
-          animate(1000, style({
-            opacity: 1
-          }))
-        ], { optional: true})
+          })
+        ], { optional: true }),
+
+        group([
+          query(':leave', [
+            animate('200ms ease-in', style ({
+              opacity: 0,
+              transform: 'translateX(-80px)'
+            }))
+          ], { optional: true }),
+  
+          query(':enter', [
+            style({
+              transform: 'translateX(80px)',
+              opacity: 0
+            }),
+            animate(200, style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            }))
+          ], { optional: true})
+        ])
+
       ])
     ]) 
   ]
