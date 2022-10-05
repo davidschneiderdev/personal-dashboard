34f25e7b4544e213bf99e1b97b6a9e67e4540e85
David Schneider <david.jay.schneider@gmail.com>
2022-10-05 10:15:28 -0400

    Create add note component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AddNoteComponent } from './add-note/add-note.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
@@ -7,7 +8,8 @@ import { NotesComponent } from './notes/notes.component';
 const routes: Routes = [
   { path: 'bookmarks', component: BookmarksComponent, data: { tab: 1 } },
   { path: 'todos', component: TodosComponent, data: { tab: 2 } },
-  { path: 'notes', component: NotesComponent, data: { tab: 3} }
+  { path: 'notes', component: NotesComponent, data: { tab: 3} },
+  { path: 'notes/add', component: AddNoteComponent }
 ];
 
 @NgModule({
