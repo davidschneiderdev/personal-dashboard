7e4d28d7869e4832f9c9a4d28c2e319788eadb91
David Schneider <david.jay.schneider@gmail.com>
2022-10-05 09:38:26 -0400

    Completes tab animation transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { transition, trigger, animate, style, query, group } from '@angular/anim
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('routeAnim', [
-      transition('* => *', [
+      transition(':increment', [
 
         query(':enter, :leave', [
           style({
@@ -24,16 +24,50 @@ import { transition, trigger, animate, style, query, group } from '@angular/anim
           query(':leave', [
             animate('200ms ease-in', style ({
               opacity: 0,
-              transform: 'translateX(-80px)'
+              transform: 'translateX(-50px)'
             }))
           ], { optional: true }),
   
           query(':enter', [
             style({
-              transform: 'translateX(80px)',
+              transform: 'translateX(50px)',
               opacity: 0
             }),
-            animate(200, style({
+            animate('250ms 100ms ease-out', style({
+              opacity: 1,
+              transform: 'translateX(0)'
+            }))
+          ], { optional: true})
+        ])
+
+      ]),
+
+      transition(':decrement', [
+
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          })
+        ], { optional: true }),
+
+        group([
+          query(':leave', [
+            animate('200ms ease-in', style ({
+              opacity: 0,
+              transform: 'translateX(50px)'
+            }))
+          ], { optional: true }),
+  
+          query(':enter', [
+            style({
+              transform: 'translateX(-50px)',
+              opacity: 0
+            }),
+            animate('250ms 100ms ease-out', style({
               opacity: 1,
               transform: 'translateX(0)'
             }))
@@ -41,15 +75,16 @@ import { transition, trigger, animate, style, query, group } from '@angular/anim
         ])
 
       ])
+
     ]) 
   ]
 })
 export class AppComponent {
 
   prepareRoute(outlet: RouterOutlet) {
-    if (outlet.isActivated) {
-      return outlet.activatedRoute.snapshot.url
-    }
+    if (outlet.isActivated) { 
+      // console.log(outlet.activatedRouteData);
+      return outlet.activatedRouteData['tab'] }
 
     return null;
   
